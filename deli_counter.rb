9d8db3d7a6cd671c1b7e05f2e56f565a3448c35e
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-30 19:49:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts # name of the person + number in the line
+end
+
+def line(name)
+  katz_deli.each_with_id do |name, order|
+  puts "The line is currently: #{order}. #{name}"
+  #if no people puts "The line is currently empty."
+
+end
+
+
+
+def now_serving()
+  puts #calls the first person and removes him from the line
+  # when no people puts "There is nobody waiting to be served!"
+end
